5a43121cdf01ab412bad75322b4d83cc571df146
Perkles <scleinaldo85@gmail.com>
2022-10-05 11:50:42 -0300

    first commit array

---
+++ desafio-array-pares/arrayPares.java
@@ -0,0 +1,18 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar:
+
+public class App {
+    public static void main(String[] args){
+
+// TODO: Crie um Array de números inteiros:
+     int[] valores = {2, 3, 5, 7, 11, 13, 18, 34};
+
+// TODO: Implemente um laço de repetição que verifique APENAS seus números Pares:
+     for (int i : valores){
+         if (i % 2 == 0){
+                 System.out.println(i);
+                } 
+            }
+           
+    }
+
+}
