129aa2dde9012d601acb94616153fe41f1054bc6
Perkles <scleinaldo85@gmail.com>
2022-10-18 20:06:34 -0300

    first commit contando números pares

---
+++ desafio-contando-números-pares/contandoNumerosPares.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class contandoNumerosPares {
+    public static void main(String[] args)
+    {
+        int num = Integer.parseInt(new Scanner(System.in).nextLine());
+        var count = 0;
+
+        for (var i = 1; i <= num; i++)
+        {
+            var strNum = String.valueOf(i);
+            if (strNum.length() == 1)
+            {
+                if (i %2 == 0 )
+                {
+                    count++;
+                }
+                continue;
+            }
+            char[] vs = strNum.toCharArray();
+            var sum = 0;
+            for (var j = 0; j < 2; j++)
+            {
+                sum += (int)Character.getNumericValue(vs[ j ]);
+            }
+            if (sum % 2 == 0)
+            {
+                count++;
+            }
+        }
+        System.out.println( count );
+    }
+}
