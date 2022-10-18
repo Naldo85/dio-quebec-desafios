33c9d94361a62e3d451386cfb1915d5a02f8c68d
Perkles <scleinaldo85@gmail.com>
2022-10-18 19:41:17 -0300

    first commit reduzir um número a zero

---
+++ desafio-reduzindo-número-a-zero/desafioReduzirNumeroaZero.java
@@ -0,0 +1,21 @@
+
+
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+import java.util.*;
+ 
+import java.util.Scanner;
+
+public class desafioReduzirNumeroaZero{
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+        var step = 0;
+//Para números impares
+        while (num != 0) {
+            if ((num % 2) != 0) num--;
+            else num/=2;
+            step++;
+        }
+        System.out.println(step);
+    }
+}
